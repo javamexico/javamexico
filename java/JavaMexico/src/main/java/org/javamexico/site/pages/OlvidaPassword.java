744129037aa73dab13f932bdd107e3789657d208
Enrique Zamudio <alterbass@gmail.com>
2010-12-08 13:36:08 -0600

    Agregar esqueletos de paginas para olvido de password, registro de usuario nuevo y cambio de password.

---
+++ java/JavaMexico/src/main/java/org/javamexico/site/pages/OlvidaPassword.java
@@ -0,0 +1,11 @@
+package org.javamexico.site.pages;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ezamudio
+ * Date: Dec 8, 2010
+ * Time: 10:53:36 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OlvidaPassword {
+}
---
+++ java/JavaMexico/src/main/java/org/javamexico/site/pages/Registro.java
@@ -0,0 +1,11 @@
+package org.javamexico.site.pages;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ezamudio
+ * Date: Dec 8, 2010
+ * Time: 10:56:34 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Registro {
+}
