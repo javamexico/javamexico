9f070de126d7eb3b1e1b4ec2cc89163dbe7a59ff
Enrique Zamudio <alterbass@gmail.com>
2010-12-16 14:54:19 -0600

    Refactoring de la prueba de base64

--- java/core/src/test/java/org/javamexico/util/TestBase64.java
+++ java/core/src/test/java/org/javamexico/util/TestBase64.java
@@ -5,24 +5,21 @@ import org.junit.Test;
 
 public class TestBase64 {
 
+	void test(byte[] data, String expected) {
+		String enc = Base64.base64Encode(data, 0, data.length);
+		Assert.assertEquals(expected, enc);
+		byte[] dec = Base64.base64Decode(enc);
+		Assert.assertArrayEquals(data, dec);
+	}
+
 	@Test
 	public void testBase64() {
 		//Codificar, comparando contra cadenas generadas por OpenSSL
-		byte[] data = "Cadena de prueba para codificacion de base640".getBytes();
-		String enc = Base64.base64Encode(data, 0, data.length - 3);
-		Assert.assertEquals("Q2FkZW5hIGRlIHBydWViYSBwYXJhIGNvZGlmaWNhY2lvbiBkZSBiYXNl", enc);
-		//Decodificar y debemos obtener la original
-		byte[] dec = Base64.base64Decode(enc);
-		enc = Base64.base64Encode(data, 0, data.length - 2);
-		Assert.assertEquals("Q2FkZW5hIGRlIHBydWViYSBwYXJhIGNvZGlmaWNhY2lvbiBkZSBiYXNlNg==", enc);
-		dec = Base64.base64Decode(enc);
-		enc = Base64.base64Encode(data, 0, data.length - 1);
-		Assert.assertEquals("Q2FkZW5hIGRlIHBydWViYSBwYXJhIGNvZGlmaWNhY2lvbiBkZSBiYXNlNjQ=", enc);
-		dec = Base64.base64Decode(enc);
-		enc = Base64.base64Encode(data, 0, data.length);
-		Assert.assertEquals("Q2FkZW5hIGRlIHBydWViYSBwYXJhIGNvZGlmaWNhY2lvbiBkZSBiYXNlNjQw", enc);
-		dec = Base64.base64Decode(enc);
-		Assert.assertArrayEquals(data, dec);
+		String data = "Cadena de prueba para codificacion de base640";
+		test(data.substring(0, data.length() - 3).getBytes(), "Q2FkZW5hIGRlIHBydWViYSBwYXJhIGNvZGlmaWNhY2lvbiBkZSBiYXNl");
+		test(data.substring(0, data.length() - 2).getBytes(), "Q2FkZW5hIGRlIHBydWViYSBwYXJhIGNvZGlmaWNhY2lvbiBkZSBiYXNlNg==");
+		test(data.substring(0, data.length() - 1).getBytes(), "Q2FkZW5hIGRlIHBydWViYSBwYXJhIGNvZGlmaWNhY2lvbiBkZSBiYXNlNjQ=");
+		test(data.getBytes(), "Q2FkZW5hIGRlIHBydWViYSBwYXJhIGNvZGlmaWNhY2lvbiBkZSBiYXNlNjQw");
 	}
 
 }
